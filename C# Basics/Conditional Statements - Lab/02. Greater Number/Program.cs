c07822bfaa2ab2d3a45dfc149acb541907accb2f
Velko <velislav.mu@gmail.com>
2026-07-24 21:12:16 +0300

    Add folder: Conditional Statements - Lab

---
+++ C# Basics/Conditional Statements - Lab/02. Greater Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Greater_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            if (num > num2)
+            {
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine(num2);
+            }
+        }
+    }
+}
---
+++ C# Basics/Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+
+            //calculations
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double result = a * a;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double result = a * b;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double result = Math.PI * r * r;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (figure == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double ha = double.Parse(Console.ReadLine());
+                double result = a * ha / 2;
+                Console.WriteLine($"{result:f3}");
+            }
+            else if (figure == "trapec")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+                double d = double.Parse(Console.ReadLine());
+                double result = (a + b);
+            }
+        }
+    }
+}
