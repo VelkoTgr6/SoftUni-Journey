1949b112aa69e1038c3c58d76574044dc8177b7d
Velko <velislav.mu@gmail.com>
2026-07-26 14:11:08 +0300

    Add folder: Generics - Lab

---
+++ C# Advanced/Generics - Lab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] strings1 = ArrayCreator.Create(5, "Pesho");
+            int[] ints = ArrayCreator.Create(5, 33);
+        }
+    }
+}
---
+++ C# Advanced/Generics - Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    internal class EqualityScale<T>
+    {
+        public T Left { get; set; }
+        public T Right { get; set; }
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public bool AreEqual()
+        {
+            return Left.Equals(Right);
+        }
+
+    }
+}
