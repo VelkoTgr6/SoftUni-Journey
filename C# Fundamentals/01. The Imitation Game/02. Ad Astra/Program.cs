0de0d56d10cc21fb6dff26720a697da8d17cbea4
Velko <velislav.mu@gmail.com>
2026-07-27 23:21:35 +0300

    Add folder: 01. The Imitation Game

---
+++ C# Fundamentals/01. The Imitation Game/02. Ad Astra/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Ad_Astra
+{
+    class Products
+    {
+        public string Name { get; set; }
+
+        public string Expiration { get; set; }
+
+        public int Calories { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\|(?<product>\w+\s\w+|\w+)\|(?<expiration>\d+/\d+/\d+)\|(?<calories>\d+)\||#(?<product>\w+\s\w+|\w+)#(?<expiration>\d+/\d+/\d+)#(?<calories>\d+)#";
+            string input = Console.ReadLine();
+            int caloriesSum = 0;
+            List<Products>products = new List<Products>();
+
+
+            foreach (Match match in Regex.Matches(input,pattern))
+            {
+                Products product = new Products();
+                product.Name = match.Groups["product"].Value;
+                product.Expiration = match.Groups["expiration"].Value;
+                product.Calories =int.Parse(match.Groups["calories"].Value);
+                caloriesSum += product.Calories;
+                products.Add(product);
+            }
+            caloriesSum /= 2000;
+            Console.WriteLine($"You have food to last you for: {caloriesSum} days!");
+            foreach (Products item in products)
+            {
+                Console.WriteLine($"Item: {item.Name}, Best before: {item.Expiration}, Nutrition: {item.Calories}");
+            }
+        }
+
+    }
+}
---
+++ C# Fundamentals/01. The Imitation Game/Final Exam Retake/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Final_Exam_Retake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input="";
+            StringBuilder sb = new StringBuilder(Console.ReadLine());
+            while((input=Console.ReadLine())!= "Decode")
+            {
+                string[] inputArr = input.Split("|", StringSplitOptions.RemoveEmptyEntries);
+                
+                switch (inputArr[0])
+                {
+                    case "Move":
+                        string firstSubstring = sb.ToString(0, int.Parse(inputArr[1]));
+                        sb.Remove(0, int.Parse(inputArr[1]));
+                        sb.Append(firstSubstring); break;
+                    case "Insert":
+                        sb.Insert(int.Parse(inputArr[1]),inputArr[2]);break;
+                    case "ChangeAll":
+                        sb.Replace(inputArr[1],inputArr[2]);break;
+                }
+            }
+            Console.WriteLine($"The decrypted message is: {sb.ToString()}");
+        }
+    }
+}
