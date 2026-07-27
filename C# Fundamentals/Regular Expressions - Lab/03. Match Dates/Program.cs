c7d60be2365824b1291afe835e4fca99ff8dd21b
Velko <velislav.mu@gmail.com>
2026-07-27 23:22:09 +0300

    Add folder: Regular Expressions - Lab

---
+++ C# Fundamentals/Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern= @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            string input = Console.ReadLine();
+            var dates=Regex.Matches(input, pattern);
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+
+        }
+    }
+}
---
+++ C# Fundamentals/Regular Expressions - Lab/Regular Expressions - Lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regular_Expressions___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)";
+            string input=Console.ReadLine();
+            MatchCollection matches=Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
