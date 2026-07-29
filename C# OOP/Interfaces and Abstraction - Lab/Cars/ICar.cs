16fe11be90be3f69020c58e5a8b0870972ee6cff
Velko <velislav.mu@gmail.com>
2026-07-29 23:08:45 +0300

    Added folder: Interfaces and Abstraction - Lab

---
+++ C# OOP/Interfaces and Abstraction - Lab/Cars/ICar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public void Start();
+        public void Stop();
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction - Lab/Cars/Tesla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar
+    {
+        public Tesla(string color, string model, int battery)
+        {
+            Battery = battery;
+            Model = model;
+            Color = color;
+        }
+
+        public int Battery { get; set; }
+        public string Model { get ; set ; }
+        public string Color { get ; set ; }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries";
+        }
+    }
+}
