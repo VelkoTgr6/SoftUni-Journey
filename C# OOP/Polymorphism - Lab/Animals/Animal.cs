39c506e8be677f2e0098d6dd6fe095cd1893328c
Velko <velislav.mu@gmail.com>
2026-07-29 23:08:56 +0300

    Added folder: Polymorphism - Lab

---
+++ C# OOP/Polymorphism - Lab/Animals/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, string favoriteFood)
+        {
+            Name = name;
+            FavoriteFood = favoriteFood;
+        }
+
+        public string Name { get; protected set; }
+        public string FavoriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my favorite food is {FavoriteFood}";
+
+        }
+    }
+}
---
+++ C# OOP/Polymorphism - Lab/Shapes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(20, 30);
+            rectangle.CalculateArea();
+            Console.WriteLine(rectangle.Draw()+rectangle.CalculateArea());
+
+            Circle circle = new Circle(20);
+            circle.CalculateArea();
+            Console.Write(circle.Draw()+circle.CalculateArea());
+        }
+    }
+}
